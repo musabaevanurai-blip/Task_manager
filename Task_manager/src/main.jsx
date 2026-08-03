0f5eeaf3f61ec54034053abc59be9a5c21a9ec1d
Adis12309 <adiskrash2@gmail.com>
2026-08-03 20:50:35 +0600

    Update main.jsx

--- Task_manager/src/main.jsx
+++ Task_manager/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -8,3 +11,4 @@ createRoot(document.getElementById("root")).render(
   </StrictMode>,
 );
 // main.jsx
+
